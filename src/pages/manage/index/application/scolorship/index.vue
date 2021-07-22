f139e71537365bbb1f9d5dd9014dd3a99ea90cc2
Joey Caughey <joeycaughey@joeys-air.lan>
2021-07-22 11:57:03 -0400

    Scolorship Form Frontend / Store

---
+++ src/pages/manage/index/application/scolorship/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-container fluid>
+    <h1>Scolorship.</h1>
+
+    <section>
+      <Question>
+        {{ $t('are_you_a_high_school_student') }}
+      </Question>
+
+      <RadioList :options="['Yes', 'No']" 
+        v-model="profile.scolorship.is_high_school_student" 
+        :value="profile.scolorship.is_high_school_student" 
+      />
+    </section>
+
+    <section v-if="profile.scolorship.is_high_school_student=='Yes'">
+      <Question>
+        {{ $t('are_you_a_high_school_graduate') }}
+      </Question>
+
+      <RadioList :options="['Yes', 'No']" 
+        v-model="profile.scolorship.is_high_school_graduate" 
+        :value="profile.scolorship.is_high_school_graduate" 
+      />
+    </section>
+    <section v-if="profile.scolorship.is_high_school_student=='Yes' && profile.scolorship.is_high_school_graduate == 'No'">
+      <Question>
+        {{ $t('are_you_pursuing_aviation') }}
+      </Question>
+
+      <RadioList :options="['Yes', 'No']" 
+        v-model="profile.scolorship.is_pursuing_aviation" 
+        :value="profile.scolorship.is_pursuing_aviation" 
+      />
+    </section>
+
+    <Buttons v-if="valid">
+      <nuxt-link to="/">Next</nuxt-link>
+    </Buttons>
+
+  </v-container>
+</template>
+
+
+
+<script>
+import { mapMutations, mapGetters } from 'vuex'
+import Question from '~/components/forms/Question.vue';
+import Buttons from '~/components/forms/Buttons.vue';
+import RadioList from '~/components/forms/RadioList.vue';
+
+export default {
+  components: {
+    Question,
+    RadioList
+  },
+  computed: {
+    ...mapGetters({
+
+    }),
+    profile: {
+      get() {
+        return this.$store.getters['profile/GET']
+      },
+      set(values) {
+        return this.$store.commit('profile/SET', values)
+      }
+    },
+    valid() {
+      return;
+    }
+  }
+}
+</script>
+
+
+<i18n>
+{
+  "en": {
+    "are_you_a_high_school_student": "Are you a grade 12 student attending a Yukon High School and entering the first year of post secondary?",
+    "are_you_a_high_school_graduate": "Are you or will you be a high school graduate from a yukon high school?",
+    "are_you_pursuing_aviation": "Are you pursuing aviation oriented technical training?"
+  },
+  "fr": {
+    "are_you_a_high_school_student": "Are you a grade 12 student attending a Yukon High School and entering the first year of post secondary?",
+    "are_you_a_high_school_graduate": "Are you or will you be a high school graduate from a yukon high school?",
+    "are_you_pursuing_aviation": "Are you pursuing aviation oriented technical training?"
+  }
+}
+</i18n>
