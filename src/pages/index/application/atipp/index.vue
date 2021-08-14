b3f1dfe0f2a418d2928ff14b94bf521885ea3c57
Joey Caughey <joeycaughey@Joeys-MacBook-Air.local>
2021-08-14 09:36:47 -0700

    ATIPP Statement + Terms Components

---
+++ src/pages/index/application/atipp/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container fluid>
+    <h2>{{ $t('title') }}</h2>
+    <p>
+      You must read and agree to the ATIPP statement. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempor urna ac nulla sollicitudin placerat. Cras condimentum ornare mauris at porttitor. Sed bibendum vehicula nulla eu molestie. 
+    </p>
+
+    <TermsWrapper v-model="read_terms">
+      <ATIPP  />
+    </TermsWrapper>
+  </v-container>
+</template>
+
+<script>
+import { mapMutations, mapGetters } from 'vuex'
+import Question from '~/components/forms/Question.vue';
+import TermsWrapper from '~/components/terms/wrapper.vue';
+import ATIPP from '~/components/terms/atipp.vue';
+
+export default {
+  components: {
+    Question,
+    TermsWrapper,
+    ATIPP
+  },
+  data() {
+    return {
+      read_terms: false
+    }
+  },
+  computed: {
+    profile: {
+      get() {
+        return this.$store.getters['profile/GET']
+      },
+      set(values) {
+        return this.$store.commit('profile/SET', values)
+      }
+    },
+    valid() {
+      var is_valid = this.read_terms
+
+      return is_valid
+    }
+  },
+  mounted() {
+    this.$emit('input', this.valid)
+  },
+  watch: {
+    valid(to, from) {
+      this.$emit('input', this.valid)
+    }
+  }
+}
+</script>
+
+
+<i18n>
+{
+  "en": {
+    "title": "ATIPP Collection Statement"
+  },
+  "fr": {
+    "title": "ATIPP Collection Statement"
+  }
+}
+</i18n>
